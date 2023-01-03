759b61c598b3b6d98871793e411d50c0728b11a2
Benjamin Phillips <benjamingt537@gmail.com>
2023-01-03 16:51:54 -0500

    added save all button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,15 @@
+let min = 8
+let max = 20
+
 function innit() {
-    generatePlanner()
+    generatePlanner(8, 20)
 }
 
 function generatePlanner() {
     // document.querySelector('.planner-container')
     let currentTime = new Date().getHours()
     console.log(currentTime)
-    for (let hour = 8; hour < 20; hour++) { // I like to create elements on one line since it takes so much space for no reason
+    for (let hour = min; hour < max; hour++) { // I like to create elements on one line since it takes so much space for no reason
         hour === currentTime ? bgColor = 'lightcoral' : hour < currentTime ? bgColor = 'white' : bgColor = 'palegreen'
         function getDisplayHour() {
             if (hour > 12) {
@@ -26,9 +29,14 @@ function generatePlanner() {
     }
 }
 
-function saveText(hour) {
-    console.log(hour)
-    localStorage.setItem(`note-hour-${hour}`, document.getElementById(hour).value)
+function saveNote(hour) {
+    if (hour === 100) {
+        for (let i = min; i < max; i++) {
+            localStorage.setItem(`note-hour-${i}`, document.getElementById(i).value)
+        }
+    } else {
+        localStorage.setItem(`note-hour-${hour}`, document.getElementById(hour).value)
+    }
 }
 
 innit()
